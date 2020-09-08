03948b4eee321aa89f6f713927de999b4b3530ef
NicoVTW <70753338+NicoVTW@users.noreply.github.com>
2020-09-08 17:53:51 +0200

    Update echo_bot.py

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -5,7 +5,7 @@
 
 import telebot
 
-API_TOKEN = '<api_token>'
+API_TOKEN = '1364773949:AAHoR-4R_yAsvPPeYisVvTndc2BMfbTwb8c'
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -14,15 +14,18 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Ti do il benvenuto alla registrazione per il gruppo "STREETCULTURE" 
+Segui le prossime istruzioni per accedere.
+Vuoi accedere come?
+CLIENTE o VENDITORE.\
 """)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    if messagge.text=='CLIENTE':
+    bot.reply_to(message, 'Stai per accedere al gruppo di compra-vendita streetwear STREETCULTURE, sarai un possibile acquirente, non potrai inviare alcun messaggio, per mantenere il gruppo libero per i prodotti in vendita, se fossi interessato ad acquistare o avere ulteriori informazioni riguardo i prodotti mostrati, dovrai solo rispondere al prodotto con un messaggio privato al venditore. Digita CONTINUA per proseguire.')
 
 
 bot.polling()
